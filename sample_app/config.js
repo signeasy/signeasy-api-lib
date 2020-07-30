c2478fbac3ce861db712e453972e4f643f189557
Rajith <rajith@signeasy.com>
2020-07-30 19:12:46 +0530

    v2

--- sample_app/config.js
+++ sample_app/config.js
@@ -1,13 +1,13 @@
 const port = 3000;
 
-const baseUrl = 'https://api-demo.getsigneasy.com';
+const baseUrl = 'https://api-demo.signeasy.com';
 
 const config = {
   baseUrl,
   port,
   appUrl: baseUrl,
-  authorizationURL: 'https://api-ext.getsigneasy.com/oauth2/authorize',
-  tokenURL: 'https://api-ext.getsigneasy.com/oauth2/token',
+  authorizationURL: 'https://api.signeasy.com/oauth2/authorize',
+  tokenURL: 'https://api.signeasy.com/oauth2/token',
   clientID: 'fiuLeRRIouFMXblt1keYYbPPbeelFjdOqC7kPmmo',
   clientSecret: 'J5UvQ41IUhaXpHGggpY0GUDF9tgz3hzDIw1koPnrcYED87dqm6',
   callbackURL: `${baseUrl}/auth/cb`,
